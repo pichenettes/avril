bfa556f52d5fb2b87333194712be97667c857ae4
pichenettes <ol.gillet@gmail.com>
2011-07-04 03:28:31 +0200

    SD Card interface, all functions for read

--- devices/sd_card.h
+++ devices/sd_card.h
@@ -34,8 +34,11 @@ enum SdCommands {
   SD_CMD_SEND_IF_COND = 8,
   SD_CMD_SEND_CSD = 9,
   SD_CMD_SEND_CID = 10,
+  SD_CMD_STOP_TRANSMISSION = 12,
   SD_CMD_SEND_STATUS = 13,
+  SD_CMD_SET_BLOCKLEN = 16,
   SD_CMD_READ_SINGLE_BLOCK = 17,
+  SD_CMD_READ_MULTIPLE_BLOCK = 18,
   SD_CMD_WRITE_BLOCK = 24,
   SD_CMD_WRITE_MULTIPLE_BLOCK = 25,
   SD_CMD_ERASE_WR_BLK_START = 32,
@@ -68,10 +71,18 @@ enum SdStatus {
   SD_ERROR_ERASE_TIMEOUT,
 };
 
+enum SdToken {
+  SD_TOKEN_DATA_RES_ACCEPTED = 0x05,
+  SD_TOKEN_WRITE_MULTIPLE = 0xfc,
+  SD_TOKEN_STOP_TRAN = 0xfd,
+  SD_TOKEN_DATA_START_BLOCK = 0xfe
+};
+
 enum SdState {
   SD_STATE_READY = 0,
   SD_STATE_IDLE = 1,
   SD_STATE_ILLEGAL_COMMAND = 4,
+  SD_STATE_START_DATA_BLOCK = 0xfe
 };
 
 enum SdCardType {
@@ -184,31 +195,36 @@ union CSD {
   CSDv2 v2;
 };
 
-struct BaseSdCardConfig {
+// A default series of timeout values for the SD card reading class.
+struct DefaultSdCardConfig {
+  enum {
+    busy_timeout = 300,
+    init_timeout = 1000,
+    erase_timeout = 2000,
+    read_timeout = 200,
+    write_timeout = 400,
+  };
+};
+
+// A config with all timer-based timeout disabled. Instead, reading will timeout
+// based on a number of retries (65535 SPI read/write cycles).
+struct NoTimerBasedTimeoutConfig {
   enum {
     busy_timeout = 0,
     init_timeout = 0,
     erase_timeout = 0,
     read_timeout = 0,
     write_timeout = 0,
-    /*busy_timeout = 300,
-    init_timeout = 1000,
-    erase_timeout = 2000,
-    read_timeout = 200,
-    write_timeout = 400,*/
   };
 };
 
-template<typename Spi, typename Config = BaseSdCardConfig>
+template<typename Spi, typename Config = NoTimerBasedTimeoutConfig>
 class SdCard {
  public:
   SdCard() { }
   
-  uint8_t Init() {
+  static SdStatus Init() {
     Spi::Init();
-    pending_read_ = 0;
-    block_offset_ = 0;
-    status_ = 0;
     type_ = 0;
     
     Spi::End();
@@ -229,17 +245,25 @@ class SdCard {
       }
     
       // Try a V2 command to determine the card version.
+      // The command checks for the 2.7-3.6V voltage range.
       if (Command(SD_CMD_SEND_IF_COND, 0x1aa) & SD_STATE_ILLEGAL_COMMAND) {
         type_ = SD_SD1;
       } else {
-        Swallow(4);
-        if (status_ != 0xaa) {
+        Spi::Receive();
+        Spi::Receive();
+        // Check that the card operates in the 2.7-3.6V range.
+        if (!(Spi::Receive() & 0x01)) {
+          return SD_ERROR_INIT;
+        }
+        // Check the test pattern.
+        if (Spi::Receive() != 0xaa) {
           return SD_ERROR_INIT;
         }
         type_ = SD_SD2;
       }
     
-      // Initialize card.
+      // Initialize card. Note that MMC cards do not accept this command.
+      // We do not support MMC cards.
       if (!WaitForAStatus<Config::init_timeout>(
           SD_AMCD_SD_SEND_OP_COND,
           type_ == SD_SD2 ? 0x40000000 : 0,
@@ -252,28 +276,133 @@ class SdCard {
         if (Command(SD_CMD_READ_OCR, 0)) {
           return SD_ERROR_INIT;
         }
-        if ((Spi::Receive() & 0xc0) == 0xc0) {
+        if ((Spi::Receive() & 0x40)) {
           type_ = SD_SDHC;
         }
         Swallow(3);
+      } else {
+        if (Command(SD_CMD_SET_BLOCKLEN, 512)) {
+          return SD_ERROR_INIT;
+        }
       }
+      return SD_OK;
+    }
+  }
+
+  static uint32_t GetNumSectors() {
+    CSD csd;
+    if (ReadCSD(&csd) != SD_OK) {
+      return 0;
+    } else {
+      if (csd.v1.csd_ver == 0) {
+        uint8_t read_bl_len = csd.v1.read_bl_len;
+        uint16_t c_size = (csd.v1.c_size_high << 10) \
+            | (csd.v1.c_size_mid << 2) | csd.v1.c_size_low;
+        uint8_t c_size_mult = (csd.v1.c_size_mult_high << 1) \
+            | csd.v1.c_size_mult_low;
+        uint8_t shift = (c_size_mult + read_bl_len - 7);
+        return static_cast<uint32_t>(c_size + 1) << shift;
+      } else if (csd.v2.csd_ver == 1) {
+        uint32_t c_size = (static_cast<uint32_t>(csd.v2.c_size_high) << 16) \
+            | (csd.v2.c_size_mid << 8) | csd.v2.c_size_low;
+        return (c_size + 1) << 10;
+      } else {
+        return 0;
+      }
+    }
+  }
+  
+  static SdStatus ReadSectors(
+      uint32_t start,
+      uint8_t num_sectors,
+      uint8_t* data) {
+    scoped_resource<Spi> spi_session;
+    if (type_ != SD_SDHC) {
+      start *= 512;
+    }
+    if (num_sectors == 1) {
+      if (Command(SD_CMD_READ_SINGLE_BLOCK, start) != 0) {
+        return SD_ERROR_READ;
+      }
+      return ReadData(data, 512);
+    } else {
+      if (Command(SD_CMD_READ_MULTIPLE_BLOCK, start) != 0) {
+        return SD_ERROR_READ;
+      }
+      do {
+        SdStatus status = ReadData(data, 512);
+        if (status != SD_OK) {
+          return status;
+        }
+        data += 512;
+      } while (--num_sectors);
+      Command(SD_CMD_STOP_TRANSMISSION, 0);
       return SD_OK;
     }
   }
   
   static inline uint8_t type() { return type_; }
+  static inline uint16_t sector_size() { return 512; }
 
  private:
+  static SdStatus ReadCSD(CSD* csd) {
+    scoped_resource<Spi> spi_session;
+    if (Command(SD_CMD_SEND_CSD, 0)) {
+      return SD_ERROR_READ_REG;
+    }
+    return ReadData((uint8_t*)(csd), 16);
+  }
+
+  static SdStatus ReadCID(CID* cid) {
+    scoped_resource<Spi> spi_session;
+    if (Command(SD_CMD_SEND_CID, 0)) {
+      return SD_ERROR_READ_REG;
+    }
+    return ReadData((uint8_t*)(cid), 16);
+  }
+  
+  static SdStatus ReadData(uint8_t* data, uint16_t size) {
+    if (WaitForData<Config::read_timeout>() != SD_STATE_START_DATA_BLOCK) {
+      return SD_ERROR_READ_TIMEOUT;
+    }
+    do {
+      *data++ = Spi::Receive();
+      *data++ = Spi::Receive();
+    } while (size -= 2);
+    Swallow(2);  // CRC
+    return SD_OK;
+  }
+  
+  /*static uint8_t WriteData(uint8_t token, const uint8_t* data) {
+    if (!WaitNotBusy<Config::busy_timeout>()) {
+      return SD_ERROR_WRITE_TIMEOUT;
+    }
+    Spi::Send(token);
+    if (token != SD_TOKEN_STOP_TRAN) {
+      uint8_t word_count = 0;
+      do {
+        Spi::Send(*data++);
+        Spi::Send(*data++);
+      } while (--word_count);
+      // Dummy CRC.
+      Spi::Send(0xff);
+      Spi::Send(0xff);
+      uint8_t response = Spi::Receive();
+      if ((response & 0x1f) != SD_TOKEN_DATA_RES_ACCEPTED) {
+        return SD_ERROR_WRITE;
+      }
+    }
+    return SD_OK;
+  }*/
+
   static void Swallow(uint8_t n) {
     for (uint8_t i = 0; i < n; ++i) {
-      status_ = Spi::Receive();
+      Spi::Receive();
     }
   }
   static uint8_t Command(uint8_t command, uint32_t argument) {
-    FlushRead();
-    
-    Spi::Begin();
-    WaitNotBusy<Config::busy_timeout>();
+    // Wait for a few clock cycles.
+    Spi::Receive();
     Spi::Send(command | 0x40);
     LongWord l;
     l.value = argument;
@@ -288,31 +417,20 @@ class SdCard {
     if (command == SD_CMD_SEND_IF_COND) {
       crc = 0x87;
     }
+    uint8_t status;
     Spi::Send(crc);
-    status_ = 0x80;
-    for (uint8_t i = 0; i < 255; i++) {
-      status_ = Spi::Receive();
-      if (!(status_ & 0x80)) {
+    for (uint8_t i = 0; i < 20; i++) {
+      status = Spi::Receive();
+      if (!(status & 0x80)) {
         break;
       }
     }
-    return status_;
+    return status;
   }
   
   static uint8_t ACommand(uint8_t command, uint32_t argument) {
     Command(SD_CMD_APP_CMD, 0);
     return Command(command, argument);
-  }
-  
-  // Read a partially block until its end.
-  static void FlushRead() {
-    if (pending_read_) {
-      while (block_offset_++ < 514) {
-        Spi::Receive();  // > /dev/null
-      }
-      Spi::End();
-      pending_read_ = 0;
-    }
   }
   
   template<uint16_t timeout>
@@ -358,10 +476,34 @@ class SdCard {
   }
   
   template<uint16_t timeout>
-  static uint8_t WaitNotBusy() {
+  static uint8_t WaitForData() {
+    uint8_t status;
     if (timeout == 0) {
       // Arbitrary timeout that doesn't use the system clock.
       for (uint16_t i = 0; i < 0xffff; ++i) {
+        status = Spi::Receive();
+        if (status != 0xff) {
+          break;
+        }
+      }
+      return status;
+    } else {
+      uint16_t now = milliseconds();
+      do {
+        status = Spi::Receive();
+        if (status != 0xff) {
+          break;
+        }
+      } while ((milliseconds() - now) < timeout);
+      return status;
+    }
+  }
+  
+  template<uint16_t timeout>
+  static uint8_t WaitNotBusy() {
+    if (timeout == 0) {
+      // Arbitrary timeout that doesn't use the system clock.
+      for (uint32_t i = 0; i < 0x1ffff; ++i) {
         if (Spi::Receive() == 0xff) {
           return 1;
         }
@@ -378,25 +520,10 @@ class SdCard {
     }
   }
   
-  static uint8_t pending_read_;
-  static uint8_t status_;
   static uint8_t type_;
-  static uint16_t block_offset_;
    
   DISALLOW_COPY_AND_ASSIGN(SdCard);
 };
-
-/* static */
-template<typename Spi, typename Config>
-uint8_t SdCard<Spi, Config>::pending_read_;
-
-/* static */
-template<typename Spi, typename Config>
-uint16_t SdCard<Spi, Config>::block_offset_;
-
-/* static */
-template<typename Spi, typename Config>
-uint8_t SdCard<Spi, Config>::status_;
 
 /* static */
 template<typename Spi, typename Config>
